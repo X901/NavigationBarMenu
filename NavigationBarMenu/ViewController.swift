c92a956ba85341d8dc2d143c279c1d3fde6be5b8
X901 <Basil@MacBook-Pro-2.local>
2017-05-08 22:16:24 +0300

    NavigationBarMenu
    Menu like in Telegram app

--- NavigationBarMenu/ViewController.swift
+++ NavigationBarMenu/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuTop: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuView: UIView!
+    
+    var check = true
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        menuTop.constant = -65
+        
+        let button =  UIButton(type: .custom)
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        button.setTitle("Menu", for: .normal)
+        button.addTarget(self, action: #selector(self.menuButton), for: .touchUpInside)
+        self.navigationItem.titleView = button
+
+        
+        menuView.layer.borderWidth = 0.5
+        menuView.layer.borderColor = UIColor.gray.cgColor
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func menuButton(button: UIButton) {
+     
+        if check == true {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.menuTop.constant = 0
+            self.view.layer.layoutIfNeeded()
 
+            self.check = false
+            
+        })
+        
+        }else {
+          
+            UIView.animate(withDuration: 0.5, animations: {
+                self.menuTop.constant = -65
+                self.view.layer.layoutIfNeeded()
+               
+                self.check = true
+
+                
+                
+            })
+            
+            
+        }
+        
+        
+        
+    }
+  
+
+    
 
 }
 
