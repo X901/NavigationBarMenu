04a777bcab1d8f0abd5fc69d4379c9f958d92b2a
X901 <Basil@macbook-pro-2.local>
2017-05-08 19:59:53 +0300

    Initial Commit

---
+++ NavigationBarMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationBarMenu
+//
+//  Created by X901 on 5/8/17.
+//  Copyright © 2017 Basil Baragabah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
